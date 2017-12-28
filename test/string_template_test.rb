c5cc33e32d848d272b7c00aebd07e72bd7461714
Akira Matsuda <ronnie@dio.jp>
2017-12-28 10:16:25 +0900

    Basic tests

--- test/string_template_test.rb
+++ test/string_template_test.rb
@@ -1,11 +1,52 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class StringTemplateTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::StringTemplate::VERSION
+  def setup
+    @view = Class.new(ActionView::Base).new(__dir__)
+    super
   end
 
-  def test_it_does_something_useful
-    assert false
+  def assert_render(expected, template, options = {})
+    result = @view.render(options.merge(inline: template, type: 'string'))
+    assert_equal expected, result
+  end
+
+  def test_no_interpolation
+    assert_render 'hello', 'hello'
+  end
+
+  def test_basic_interpolation
+    assert_render 'hello', '#{"hello"}'
+  end
+
+  def test_ivar
+    @view.instance_variable_set :@foo, 'hello'
+    assert_render 'hello', '#{@foo}'
+  end
+
+  def test_if
+    assert_render 'hello', '#{if true;"hello";end}'
+    assert_render 'hello', '#{"hello" if true}'
+  end
+
+  def test_each_as_map_and_join
+    assert_render "hello\nhello\nhello",
+      '#{[*1..3].map do
+        "hello"
+       end.join("\n")}'
+  end
+
+  def test_each_with_object
+    assert_render "hello\nhello\nhello\n",
+      '#{[*1..3].each_with_object("".dup) do |i, s|
+        s << "hello\n"
+       end}'
+  end
+
+  def test_render_partial
+    result = @view.render(template: 'main', handlers: 'string')
+    assert_equal 'hello, world!', result.strip
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require 'rails'
+require 'action_view'
 require "string_template"
+StringTemplate::Railtie.run_initializers
+require 'action_view/base'
 
 require "minitest/autorun"
